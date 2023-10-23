669c00853f09666f38c60e023d50cddec2262608
William Kennedy <bill@ardanlabs.com>
2023-10-23 13:27:24 -0400

    commit 2

---
+++ app/services/sales-api/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/ardanlabs/service/foundation/logger"
+)
+
+func main() {
+	var log *logger.Logger
+
+	events := logger.Events{
+		Error: func(ctx context.Context, r logger.Record) {
+			log.Info(ctx, "******* SEND ALERT ******")
+		},
+	}
+
+	traceIDFunc := func(ctx context.Context) string {
+		return ""
+	}
+
+	log = logger.NewWithEvents(os.Stdout, logger.LevelInfo, "SALES-API", traceIDFunc, events)
+
+	// -------------------------------------------------------------------------
+
+	ctx := context.Background()
+
+	if err := run(ctx, log); err != nil {
+		log.Error(ctx, "startup", "msg", err)
+		return
+	}
+}
+
+func run(ctx context.Context, log *logger.Logger) error {
+
+	// -------------------------------------------------------------------------
+	// GOMAXPROCS
+
+	log.Info(ctx, "startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// -------------------------------------------------------------------------
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	sig := <-shutdown
+
+	log.Info(ctx, "shutdown", "status", "shutdown started", "signal", sig)
+	defer log.Info(ctx, "shutdown", "status", "shutdown complete", "signal", sig)
+
+	return nil
+}
---
+++ makefile
@@ -0,0 +1,6 @@
+# Check to see if we can use ash, in Alpine images, or default to BASH.
+SHELL_PATH = /bin/ash
+SHELL = $(if $(wildcard $(SHELL_PATH)),/bin/ash,/bin/bash)
+
+run:
+	go run app/services/sales-api/main.go | go run app/tooling/logfmt/main.go
