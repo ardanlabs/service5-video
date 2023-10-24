c767d3e009db84dc7ce05f5e09a65c56c2c3b5a2
William Kennedy <bill@ardanlabs.com>
2023-10-24 13:35:19 -0400

    commit 21

--- app/services/sales-api/v1/handlers/hackgrp/hackgrp.go
+++ app/services/sales-api/v1/handlers/hackgrp/hackgrp.go
@@ -1,16 +1,17 @@
 package hackgrp
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 )
 
-func Hack(w http.ResponseWriter, r *http.Request) {
+func Hack(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	status := struct {
 		Status string
 	}{
 		Status: "OK",
 	}
 
-	json.NewEncoder(w).Encode(status)
+	return json.NewEncoder(w).Encode(status)
 }
--- foundation/web/web.go
+++ foundation/web/web.go
@@ -1,10 +1,16 @@
 package web
 
 import (
+	"context"
+	"net/http"
 	"os"
 
 	"github.com/dimfeld/httptreemux/v5"
 )
+
+// A Handler is a type that handles a http request within our own little mini
+// framework.
+type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request) error
 
 // App is the entrypoint into our application and what configures our context
 // object for each of our http handlers. Feel free to add any configuration
@@ -20,4 +26,23 @@ func NewApp(shutdown chan os.Signal) *App {
 		ContextMux: httptreemux.NewContextMux(),
 		shutdown:   shutdown,
 	}
+}
+
+// Handle sets a handler function for a given HTTP method and path pair
+// to the application server mux.
+func (a *App) Handle(method string, path string, handler Handler) {
+
+	h := func(w http.ResponseWriter, r *http.Request) {
+
+		// ADD ANY LOGIC HERE
+
+		if err := handler(r.Context(), w, r); err != nil {
+			// NOT SURE WHAT TO DO
+			return
+		}
+
+		// ADD ANY LOGIC HERE
+	}
+
+	a.ContextMux.Handle(method, path, h)
 }
