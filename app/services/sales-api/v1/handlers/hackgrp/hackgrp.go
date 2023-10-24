669b9e78e9560143edde4da7bdf9cb8cb8d11f27
William Kennedy <bill@ardanlabs.com>
2023-10-24 15:20:59 -0400

    commit 26

--- app/services/sales-api/v1/handlers/hackgrp/hackgrp.go
+++ app/services/sales-api/v1/handlers/hackgrp/hackgrp.go
@@ -2,12 +2,19 @@ package hackgrp
 
 import (
 	"context"
+	"errors"
+	"math/rand"
 	"net/http"
 
+	"github.com/ardanlabs/service/business/web/v1/response"
 	"github.com/ardanlabs/service/foundation/web"
 )
 
 func Hack(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	if n := rand.Intn(100) % 2; n == 0 {
+		return response.NewError(errors.New("TRUST ERROR"), http.StatusBadRequest)
+	}
+
 	status := struct {
 		Status string
 	}{
---
+++ business/web/v1/mid/errors.go
@@ -0,0 +1,57 @@
+package mid
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/ardanlabs/service/business/web/v1/response"
+	"github.com/ardanlabs/service/foundation/logger"
+	"github.com/ardanlabs/service/foundation/web"
+)
+
+// Errors handles errors coming out of the call chain. It detects normal
+// application errors which are used to respond to the client in a uniform way.
+// Unexpected errors (status >= 500) are logged.
+func Errors(log *logger.Logger) web.Middleware {
+	m := func(handler web.Handler) web.Handler {
+		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+			if err := handler(ctx, w, r); err != nil {
+				log.Error(ctx, "message", "msg", err)
+
+				var er response.ErrorDocument
+				var status int
+
+				switch {
+				case response.IsError(err):
+					reqErr := response.GetError(err)
+					er = response.ErrorDocument{
+						Error: reqErr.Error(),
+					}
+					status = reqErr.Status
+
+				default:
+					er = response.ErrorDocument{
+						Error: http.StatusText(http.StatusInternalServerError),
+					}
+					status = http.StatusInternalServerError
+				}
+
+				if err := web.Respond(ctx, w, er, status); err != nil {
+					return err
+				}
+
+				// If we receive the shutdown err we need to return it
+				// back to the base handler to shut down the service.
+				if web.IsShutdown(err) {
+					return err
+				}
+			}
+
+			return nil
+		}
+
+		return h
+	}
+
+	return m
+}
