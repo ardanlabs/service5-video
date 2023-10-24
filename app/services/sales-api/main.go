dedaf89bd42c974dcb437b2102bf909e6434801e
William Kennedy <bill@ardanlabs.com>
2023-10-24 11:27:08 -0400

    commit 11

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -57,7 +57,7 @@ func run(ctx context.Context, log *logger.Logger) error {
 			ReadTimeout     time.Duration `conf:"default:5s"`
 			WriteTimeout    time.Duration `conf:"default:10s"`
 			IdleTimeout     time.Duration `conf:"default:120s"`
-			ShutdownTimeout time.Duration `conf:"default:20s"`
+			ShutdownTimeout time.Duration `conf:"default:20s,mask"`
 			APIHost         string        `conf:"default:0.0.0.0:3000"`
 			DebugHost       string        `conf:"default:0.0.0.0:4000"`
 		}
@@ -77,6 +77,18 @@ func run(ctx context.Context, log *logger.Logger) error {
 		}
 		return fmt.Errorf("parsing config: %w", err)
 	}
+
+	// -------------------------------------------------------------------------
+	// App Starting
+
+	log.Info(ctx, "starting service", "version", build)
+	defer log.Info(ctx, "shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Info(ctx, "startup", "config", out)
 
 	// -------------------------------------------------------------------------
 
