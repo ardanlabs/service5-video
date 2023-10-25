028688e52d253921ec4cdd7ff7610359fd675fe5
William Kennedy <bill@ardanlabs.com>
2023-10-25 11:54:32 -0400

    commit 31

--- app/tooling/sales-admin/main.go
+++ app/tooling/sales-admin/main.go
@@ -8,28 +8,78 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func main() {
-	err := genkey()
+	err := gentoken()
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
-func genkey() error {
+func gentoken() error {
+	privateKey, err := genkey()
+	if err != nil {
+		return fmt.Errorf("generating key: %w", err)
+	}
+
+	// Generating a token requires defining a set of claims. In this applications
+	// case, we only care about defining the subject and the user in question and
+	// the roles they have on the database. This token will expire in a year.
+	//
+	// iss (issuer): Issuer of the JWT
+	// sub (subject): Subject of the JWT (the user)
+	// aud (audience): Recipient for which the JWT is intended
+	// exp (expiration time): Time after which the JWT expires
+	// nbf (not before time): Time before which the JWT must not be accepted for processing
+	// iat (issued at time): Time at which the JWT was issued; can be used to determine age of the JWT
+	// jti (JWT ID): Unique identifier; can be used to prevent the JWT from being replayed (allows a token to be used only once)
+	claims := struct {
+		jwt.RegisteredClaims
+		Roles []string
+	}{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Subject:   "12345678789",
+			Issuer:    "service project",
+			ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(8760 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		},
+		Roles: []string{"ADMIN"},
+	}
+
+	method := jwt.GetSigningMethod(jwt.SigningMethodRS256.Name)
+
+	token := jwt.NewWithClaims(method, claims)
+	token.Header["kid"] = "54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"
+
+	str, err := token.SignedString(privateKey)
+	if err != nil {
+		return fmt.Errorf("signing token: %w", err)
+	}
+
+	fmt.Println("****************")
+	fmt.Println(str)
+	fmt.Println("****************")
+
+	return nil
+}
+
+func genkey() (*rsa.PrivateKey, error) {
 
 	// Generate a new private key.
 	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
-		return fmt.Errorf("generating key: %w", err)
+		return nil, fmt.Errorf("generating key: %w", err)
 	}
 
 	// Create a file for the private key information in PEM form.
 	privateFile, err := os.Create("private.pem")
 	if err != nil {
-		return fmt.Errorf("creating private file: %w", err)
+		return nil, fmt.Errorf("creating private file: %w", err)
 	}
 	defer privateFile.Close()
 
@@ -41,7 +91,7 @@ func genkey() error {
 
 	// Write the private key to the private key file.
 	if err := pem.Encode(privateFile, &privateBlock); err != nil {
-		return fmt.Errorf("encoding to private file: %w", err)
+		return nil, fmt.Errorf("encoding to private file: %w", err)
 	}
 
 	// =========================================================================
@@ -49,14 +99,14 @@ func genkey() error {
 	// Create a file for the public key information in PEM form.
 	publicFile, err := os.Create("public.pem")
 	if err != nil {
-		return fmt.Errorf("creating public file: %w", err)
+		return nil, fmt.Errorf("creating public file: %w", err)
 	}
 	defer publicFile.Close()
 
 	// Marshal the public key from the private key to PKIX.
 	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
 	if err != nil {
-		return fmt.Errorf("marshaling public key: %w", err)
+		return nil, fmt.Errorf("marshaling public key: %w", err)
 	}
 
 	// Construct a PEM block for the public key.
@@ -67,10 +117,10 @@ func genkey() error {
 
 	// Write the public key to the public key file.
 	if err := pem.Encode(publicFile, &publicBlock); err != nil {
-		return fmt.Errorf("encoding to public file: %w", err)
+		return nil, fmt.Errorf("encoding to public file: %w", err)
 	}
 
 	fmt.Println("private and public key files generated")
 
-	return nil
+	return privateKey, nil
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.21.3
 require (
 	github.com/ardanlabs/conf/v3 v3.1.7
 	github.com/dimfeld/httptreemux/v5 v5.5.0
+	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/google/uuid v1.3.1
 )
